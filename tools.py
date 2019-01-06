7d16b1a6d8d11c4f5f301456f7bda740ed28aaf3
Haotian Chen <haochen@zynga.com>
2019-01-06 14:14:11 -0500

    inner products:

---
+++ tools.py
@@ -0,0 +1,6 @@
+def summation(list):
+    sum = 0;
+    for i in range(len(list)):
+        sum += lsit[i];
+    return sum
+            
--- vector.py
+++ vector.py
@@ -1,3 +1,6 @@
+import tools
+import math
+
 class Vector(object):
     def __init__(self, coordinates):
         try:
@@ -37,3 +40,23 @@ class Vector(object):
         for i in range(len(self.coordinates)):
             product.append(self.coordinates[i] * s)
         return Vector(product)
+
+    def magnitude(self):
+        sum = 0;
+        for i in range(len(self.coordinates)):
+            sum += math.pow(self.coordinates[i], 2);
+        return math.sqrt(sum)
+
+    def unit_vector(self):
+        mag = self.magnitude()
+        return self.scalar_mult(1/mag)
+
+    def dot(self, v):
+        dot_product = 0
+        for i in range(len(self.coordinates)):
+            dot_product += self.coordinates[i]*v.coordinates[i]
+        return dot_product
+
+    def theta(self, v):
+        dot_product = self.dot(v)
+        return math.acos(dot_product / (self.magnitude() * v.magnitude())) 
